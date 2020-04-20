183a69886ef839251277026e38a684aa9b7b8c90
user1 <user1@DESKTOP-QD9ND2F>
2020-04-20 11:25:23 +0900

    예제연습2

---
+++ day03/src/basic/Test02.java
@@ -0,0 +1,13 @@
+package basic;
+
+import java.lang.*;
+
+public class Test02 {
+	public static void main(String[] args) {
+//		자장면 한그릇 5000원, 짬뽕, 한그릇 6000원일 때
+//		자장면2그릇, 짬뽕3그릇 가격
+
+	System.out.println((5000*2)+(6000*3));	
+	
+	}
+}
