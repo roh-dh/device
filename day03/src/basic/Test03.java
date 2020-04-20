db2a55dc38069115e6f49591ae4d41a805f585f0
user1 <user1@DESKTOP-QD9ND2F>
2020-04-20 11:24:58 +0900

    예제 연습

---
+++ day03/src/basic/Test03.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class Test03 {
+	public static void main(String[] args) {
+//		독서실 이용료가 하루에 8천원입니다.
+//    	한달(30일) 이용권을 구매하면 추가로 5천원을 할인해 줍니다.
+//		1달 이용권 구매 시 할인 전 금액을 구하여 화면에 출력
+//		1달 이용권 구매 시 할인 후 금액을 구하여 화면에 출력
+		
+		System.out.println(8000*30);
+		System.out.println(8000*30-5000);
+		
+		
+	}
+}
