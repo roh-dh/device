5cd13e9b2e509accc2d445d9f8c0303e1dc0ff2d
user1 <user1@DESKTOP-QD9ND2F>
2020-04-20 09:59:08 +0900

    day03

---
+++ day03/src/basic/Test01.java
@@ -0,0 +1,7 @@
+package basic; // 파일위치(반드시 작성)
+import java.lang.*;  // import java.lang.*;
+public class Test01 { 
+	public static void main(String[] args) { 
+	}
+
+} //public class test01 {메인 메소드}클래스 기본양식
