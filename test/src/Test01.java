46cebb191687160e3ca74bf7c12e592fa3b76e3b
roh-dh <ndh13399@naver.com>
2020-05-08 18:20:17 +0900

    day14

--- test/src/Test01.java
+++ test/src/Test01.java
@@ -1,9 +1,11 @@
 import oop.static4.Robot;
 
+
 public class Test01 {
 public static void main(String[] args) {
 	//불러온 라이브러리 kh.jar에 있는 Calculator라는 클래스의 내용응 사용하겠다.
 	
+	 
 	int dice = Robot.dice();
 	System.out.println("주사위 : "+dice);
 }
